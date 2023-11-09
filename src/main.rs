680b347cf3daeaf314c11e801883fc736c11cc1c
Shintaro Shimada <87416336+Esu0@users.noreply.github.com>
2023-11-09 09:42:08 +0000

    main関数を追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 mod bignum;
 
-fn main() {}
+use rand::Rng;
+use std::time::Instant;
+
+fn main() {
+    time();
+}
+
+fn time() {
+    let n = 1 << 22;
+    let mut rng = rand::thread_rng();
+    let a: Vec<i64> = (0..n).map(|_| rng.gen_range(0..1000000000)).collect();
+    let b: Vec<i64> = (0..n).map(|_| rng.gen_range(0..1000000000)).collect();
+    let s = Instant::now();
+    let v = bignum::number_theoretic::convolution::convolution_level2(a.into_iter(), b.into_iter(), n, n);
+    println!("elapsed: {} ms", s.elapsed().as_micros() as f32 / 1000.);
+}
